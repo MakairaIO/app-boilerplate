1827e624c650fdffab401b4f4c63ef91d2adcb90
thuan.le <thuan.le@nfq.asia>
2023-02-09 13:50:56 +0700

    MAKAIRA-3264: update table

--- src/Components/TableComponent.php
+++ src/Components/TableComponent.php
@@ -10,18 +10,14 @@ class TableComponent
 {
   public array $columns =  [];
   public array $rows = [];
-  // public array $columns =  [
-  //   ['title' => 'Column 1', 'component' => ''],
-  //   ['title' => 'Column 2', 'component' => ''],
-  //   ['title' => 'Column 3', 'component' => ''],
-  //   ['title' => 'Column 4', 'component' => 'component'],
-  // ];
+  public bool $loading = false;
+  public string $wrapperClasses = 'mk-table__wrapper';
 
-  // public array $rows = [
-  //   ['row1 - col1', 'row1 - col2', 'row1 - col3', ['component' => 'button', 'variant' => 'primary', 'children' => 'Primary', 'loading' => true, 'disabled' => false, 'icon' => 'star']],
-  //   ['row2 - col1', 'row2 - col2', 'row2 - col3', ['component' => 'button', 'variant' => 'primary', 'children' => 'Primary', 'loading' => true, 'disabled' => false, 'icon' => 'star']],
-  //   ['row3 - col1', 'row3 - col2', 'row3 - col3', ['component' => 'button', 'variant' => 'secondary', 'children' => 'Click me', 'loading' => false, 'disabled' => false, 'icon' => '']],
-  //   ['row4 - col1', 'row4 - col2', 'row4 - col3', ['component' => 'textarea', 'name' => 'area_demo', 'placeholder' => 'placeholder', 'title' => 'title',]],
-  //   ['row5 - col1', 'row5 - col2', ],
-  // ];
+  public function mount( bool $loading = false)
+  {
+    if ($loading) {
+      $this->wrapperClasses = $this->wrapperClasses . " mk-table__wrapper--loading";
+      $this->loading = $loading;
+    }
+  }
 }
